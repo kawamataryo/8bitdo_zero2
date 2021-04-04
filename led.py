996238db85d0e275766abe7442158c7a9e8e19f6
kawamataryou <ba068082@gmail.com>
2021-04-04 09:00:03 +0900

    [from now] 2021/04/04 09:00:03
    diff --git a/LED.fzz b/LED.fzz
    new file mode 100644
    index 0000000..0e260dd
    Binary files /dev/null and b/LED.fzz differ
    diff --git a/Motor.fzz b/Motor.fzz
    new file mode 100644
    index 0000000..7102ea7
    Binary files /dev/null and b/Motor.fzz differ
    diff --git a/led.py b/led.py
    index d764048..db8d422 100644
    --- a/led.py
    +++ b/led.py
    @@ -1,27 +1,61 @@
    -from evdev import InputDevice, categorize, ecodes
    +import struct
     from gpiozero import LED, LEDBarGraph
    -from time import sleep
    
    -gamepad = InputDevice('/dev/input/event0')
    +# 8BitDo path
    +device_path = "/dev/input/js0"
    
    -print(gamepad)
    +# event
    +EVENT_FORMAT = "LhBB"
    +EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
    
    +# btn
    +class BtnCode:
    +  Y_ON = [1, 1, 2]
    +  Y_OFF =  [0, 1, 2]
    +  X_ON = [1, 1, 3]
    +  X_OFF = [0, 1, 3]
    +  A_ON = [1, 1, 1]
    +  A_OFF = [0, 1, 1]
    +  B_ON = [1, 1, 0]
    +  B_OFF = [0, 1, 0]
    +
    +# led
     red = LED(17)
     yellow = LED(22)
     blue = LED(10)
     white = LED(11)
    
    -class Btn:
    -  A = 305
    -  B = 304
    -  X = 307
    -  Y = 306
    -  R = 309
    -  L = 308
    -  START = 313
    -  SELECT = 312
    +with open(device_path, "rb") as device:
    +  while True:
    +    event = device.read(EVENT_SIZE)
    +    (_time, val, type, num) = struct.unpack(EVENT_FORMAT, event)
    +    code = [val, type, num]
    +
    +    if code == BtnCode.Y_ON:
    +        print("Y ON")
    +        red.on()
    +    if code == BtnCode.Y_OFF:
    +        print("Y ON")
    +        red.off()
    +    if code == BtnCode.X_ON:
    +        print("X ON")
    +        yellow.on()
    +    if code == BtnCode.X_OFF:
    +        print("X OFF")
    +        yellow.off()
    +    if code == BtnCode.A_ON:
    +        print("A ON")
    +        blue.on()
    +    if code == BtnCode.A_OFF:
    +        print("A OFF")
    +        blue.off()
    +    if code == BtnCode.B_ON:
    +        print("B ON")
    +        white.on()
    +    if code == BtnCode.B_OFF:
    +        print("B ON")
    +        white.off()
    
    -#evdev takes care of polling the controller in a loop
     for event in gamepad.read_loop():
         if event.type == ecodes.EV_KEY and event.value == 1:
             if event.code == Btn.A:
    @@ -35,12 +69,4 @@ for event in gamepad.read_loop():
                 blue.blink(n=1)
             elif event.code == Btn.Y:
                 print("Y")
    -            white.blink(n=1)
    -        elif event.code == Btn.R:
    -            print("R")
    -        elif event.code == Btn.L:
    -            print("L")
    -        elif event.code == Btn.START:
    -            print("START")
    -        elif event.code == Btn.SELECT:
    -            print("SELECT")
    +            white.blink(n=1)
    \ No newline at end of file

--- led.py
+++ led.py
@@ -1,27 +1,61 @@
-from evdev import InputDevice, categorize, ecodes
+import struct
 from gpiozero import LED, LEDBarGraph
-from time import sleep
 
-gamepad = InputDevice('/dev/input/event0')
+# 8BitDo path
+device_path = "/dev/input/js0"
 
-print(gamepad)
+# event
+EVENT_FORMAT = "LhBB"
+EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
 
+# btn
+class BtnCode:
+  Y_ON = [1, 1, 2]
+  Y_OFF =  [0, 1, 2]
+  X_ON = [1, 1, 3]
+  X_OFF = [0, 1, 3]
+  A_ON = [1, 1, 1]
+  A_OFF = [0, 1, 1]
+  B_ON = [1, 1, 0]
+  B_OFF = [0, 1, 0]
+
+# led
 red = LED(17)
 yellow = LED(22)
 blue = LED(10)
 white = LED(11)
 
-class Btn:
-  A = 305
-  B = 304
-  X = 307
-  Y = 306
-  R = 309
-  L = 308
-  START = 313
-  SELECT = 312
+with open(device_path, "rb") as device:
+  while True:
+    event = device.read(EVENT_SIZE)
+    (_time, val, type, num) = struct.unpack(EVENT_FORMAT, event)
+    code = [val, type, num]
 
-#evdev takes care of polling the controller in a loop
+    if code == BtnCode.Y_ON:
+        print("Y ON")
+        red.on()
+    if code == BtnCode.Y_OFF:
+        print("Y ON")
+        red.off()
+    if code == BtnCode.X_ON:
+        print("X ON")
+        yellow.on()
+    if code == BtnCode.X_OFF:
+        print("X OFF")
+        yellow.off()
+    if code == BtnCode.A_ON:
+        print("A ON")
+        blue.on()
+    if code == BtnCode.A_OFF:
+        print("A OFF")
+        blue.off()
+    if code == BtnCode.B_ON:
+        print("B ON")
+        white.on()
+    if code == BtnCode.B_OFF:
+        print("B ON")
+        white.off()
+
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY and event.value == 1:
         if event.code == Btn.A:
@@ -36,11 +70,3 @@ for event in gamepad.read_loop():
         elif event.code == Btn.Y:
             print("Y")
             white.blink(n=1)
-        elif event.code == Btn.R:
-            print("R")
-        elif event.code == Btn.L:
-            print("L")
-        elif event.code == Btn.START:
-            print("START")
-        elif event.code == Btn.SELECT:
-            print("SELECT")
