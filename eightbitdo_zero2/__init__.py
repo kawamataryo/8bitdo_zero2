e5842f5f62eb0b844ee6a3cfc4678d7e38bbfd1e
kawamataryou <ba068082@gmail.com>
2021-05-04 20:13:49 +0900

    [from now] 2021/05/04 20:13:49
    diff --git a/eightbitdo_zero2/__init__.py b/eightbitdo_zero2/__init__.py
    index e69de29..3798884 100644
    --- a/eightbitdo_zero2/__init__.py
    +++ b/eightbitdo_zero2/__init__.py
    @@ -0,0 +1,13 @@
    +from .eightbitdo_zero2 import *
    +
    +__copyright__ = 'Copyright (C) 2021 Kawamata Ryo'
    +__version__ = '0.1.0'
    +__license__ = 'BSD-3-Clause'
    +__author__ = 'Your Name'
    +__author_email__ = 'Your@Email'
    +__url__ = 'http://github.com/account/repository'
    +
    +__all__ = [
    +    'BtnCode',
    +    'EightBitDoZero2'
    +]

---
+++ eightbitdo_zero2/__init__.py
@@ -0,0 +1,13 @@
+from .eightbitdo_zero2 import *
+
+__copyright__ = 'Copyright (C) 2021 Kawamata Ryo'
+__version__ = '0.1.0'
+__license__ = 'BSD-3-Clause'
+__author__ = 'Your Name'
+__author_email__ = 'Your@Email'
+__url__ = 'http://github.com/account/repository'
+
+__all__ = [
+    'BtnCode',
+    'EightBitDoZero2'
+]
