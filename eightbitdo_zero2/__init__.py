753c9849826efb4348eef67207f3170027b65fcf
kawamataryou <ba068082@gmail.com>
2021-05-04 20:18:32 +0900

    [from now] 2021/05/04 20:18:32
    diff --git a/eightbitdo_zero2/__init__.py b/eightbitdo_zero2/__init__.py
    index 3798884..b81f140 100644
    --- a/eightbitdo_zero2/__init__.py
    +++ b/eightbitdo_zero2/__init__.py
    @@ -2,10 +2,10 @@ from .eightbitdo_zero2 import *
    
     __copyright__ = 'Copyright (C) 2021 Kawamata Ryo'
     __version__ = '0.1.0'
    -__license__ = 'BSD-3-Clause'
    -__author__ = 'Your Name'
    -__author_email__ = 'Your@Email'
    -__url__ = 'http://github.com/account/repository'
    +__license__ = 'MIT'
    +__author__ = 'Kawamata Ryo'
    +__author_email__ = 'ba068082@gmail.com'
    +__url__ = 'https://github.com/kawamataryo/8bitdo-zero2'
    
     __all__ = [
         'BtnCode',

--- eightbitdo_zero2/__init__.py
+++ eightbitdo_zero2/__init__.py
@@ -2,10 +2,10 @@ from .eightbitdo_zero2 import *
 
 __copyright__ = 'Copyright (C) 2021 Kawamata Ryo'
 __version__ = '0.1.0'
-__license__ = 'BSD-3-Clause'
-__author__ = 'Your Name'
-__author_email__ = 'Your@Email'
-__url__ = 'http://github.com/account/repository'
+__license__ = 'MIT'
+__author__ = 'Kawamata Ryo'
+__author_email__ = 'ba068082@gmail.com'
+__url__ = 'https://github.com/kawamataryo/8bitdo-zero2'
 
 __all__ = [
     'BtnCode',
