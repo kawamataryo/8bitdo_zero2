a37ec58f7aa2ce73279e9d39907e8b3932238720
kawamataryou <ba068082@gmail.com>
2021-05-05 10:38:19 +0900

    [from now] 2021/05/05 10:38:19
    diff --git a/eightbitdo_zero2/__init__.py b/eightbitdo_zero2/__init__.py
    index b81f140..25f94e5 100644
    --- a/eightbitdo_zero2/__init__.py
    +++ b/eightbitdo_zero2/__init__.py
    @@ -1,11 +1,11 @@
     from .eightbitdo_zero2 import *
    
     __copyright__ = 'Copyright (C) 2021 Kawamata Ryo'
    -__version__ = '0.1.0'
    +__version__ = '0.1.1'
     __license__ = 'MIT'
     __author__ = 'Kawamata Ryo'
     __author_email__ = 'ba068082@gmail.com'
    -__url__ = 'https://github.com/kawamataryo/8bitdo-zero2'
    +__url__ = 'https://github.com/kawamataryo/8bitdo_zero2'
    
     __all__ = [
         'BtnCode',
    diff --git a/setup.py b/setup.py
    index b3610bf..8f4cddc 100644
    --- a/setup.py
    +++ b/setup.py
    @@ -5,23 +5,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
    
     setuptools.setup(
         name="eightbitdo_zero2",
    -    version="0.1.0",
    +    version="0.1.1",
         author="KawamataRyo",
         author_email="ba068082@gmail.com",
         license="MIT",
         description="Python code to use thev 8bitdo zero2 controller with a Raspberry Pi",
         long_description=long_description,
         long_description_content_type="text/markdown",
    -    url="https://github.com/kawamataryo/8bitdo-zero2",
    +    url="https://github.com/kawamataryo/8bitdo_zero2",
         keywords=["8bitdo", "raspberry Pi"],
         project_urls={
    -        "Homepage": "https://github.com/kawamataryo/8bitdo-zero2",
    -        "Bug Tracker": "https://github.com/kawamataryo/8bitdo-zero2/issues",
    +        "Homepage": "https://github.com/kawamataryo/8bitdo_zero2",
    +        "Bug Tracker": "https://github.com/kawamataryo/8bitdo_zero2/issues",
         },
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
    +    packages=['eightbitdo_zero2'],
         python_requires=">=3.6",
     )

--- eightbitdo_zero2/__init__.py
+++ eightbitdo_zero2/__init__.py
@@ -1,11 +1,11 @@
 from .eightbitdo_zero2 import *
 
 __copyright__ = 'Copyright (C) 2021 Kawamata Ryo'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __license__ = 'MIT'
 __author__ = 'Kawamata Ryo'
 __author_email__ = 'ba068082@gmail.com'
-__url__ = 'https://github.com/kawamataryo/8bitdo-zero2'
+__url__ = 'https://github.com/kawamataryo/8bitdo_zero2'
 
 __all__ = [
     'BtnCode',
--- setup.py
+++ setup.py
@@ -5,23 +5,24 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="eightbitdo_zero2",
-    version="0.1.0",
+    version="0.1.1",
     author="KawamataRyo",
     author_email="ba068082@gmail.com",
     license="MIT",
     description="Python code to use thev 8bitdo zero2 controller with a Raspberry Pi",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kawamataryo/8bitdo-zero2",
+    url="https://github.com/kawamataryo/8bitdo_zero2",
     keywords=["8bitdo", "raspberry Pi"],
     project_urls={
-        "Homepage": "https://github.com/kawamataryo/8bitdo-zero2",
-        "Bug Tracker": "https://github.com/kawamataryo/8bitdo-zero2/issues",
+        "Homepage": "https://github.com/kawamataryo/8bitdo_zero2",
+        "Bug Tracker": "https://github.com/kawamataryo/8bitdo_zero2/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    packages=['eightbitdo_zero2'],
     python_requires=">=3.6",
 )
